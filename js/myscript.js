d8bae2a85b7b54b47f90ceafd3a97864eef19db1
liky12 <gayoung0703@naver.com>
2020-05-25 16:08:30 +0900

    jQurey 메뉴 수정

--- js/myscript.js
+++ js/myscript.js
@@ -3,9 +3,25 @@
 //     $(this).parent().siblings().children(".dep2box").stop().slideUp(400)        
 // })
 
+var flag = true;
+$(window).resize(function(){
+    // var winWidth=$(this).innerWidth()
+    // console.log(winWidth) 윈도우의 사이즈가 콘솔영역에 나타남
+    var winWidth=$(this).width()
+    if( winWidth > 799 &&  flag){
+        $("#header").removeClass("on")
+        flag = false;
+    }
+})
+
+
 $(".depth1 > li > a").on("mouseover focus", function(){
-    $(this).next().stop().fadeIn(400)
-    $(this).parent().siblings().children(".dep2box").stop().fadeOut(400)
+    if($(this).parents("#header").hasClass("on")){
+        $(this).next().css({display:"none"})
+    } else{  
+        $(this).next().stop().fadeIn(400)
+        $(this).parent().siblings().children(".dep2box").stop().fadeOut(400)
+    }
 })
 
 $(".depth1 > li ").on("mouseleave" , function(){
@@ -17,8 +33,11 @@ $(".depth1 >li:last   .depth2 > li:last >a").on("blur" , function(){
 })
 
 $("#header .openMOgnb").on("click", function(){
-    $(this).parents("#header").addClass("on")
+    // $(this).parents("#header").addClass("on")
     // toggleClass("on") 하면 열었다 닫았다 할 수 있다.
+    if(!$(this).parents("#header").hasClass("on")){
+        $(this).parents("#header").addClass("on")
+    }
 })
 $("#header .closeMOgnb").on("click", function(){
     $(this).parents("#header").removeClass("on")
